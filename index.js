8e067fc1a3061d37229314184e1df6e3009d8477
pavel.genuine <pavel.play@gmail.com>
2022-07-26 08:41:56 +0600

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-const { client } = require('./controllers/promo.controllers');
-const { promoRouter } = require('./routes/promo.router');
-
-
 
 const express = require('express');
 const cors = require('cors');
@@ -17,32 +13,7 @@ app.use(express.json());
 
 
 const http = require('http');
-const { Server } = require('socket.io');
 const expressServer = http.createServer(app)
-
-
-
-
-const io = new Server(expressServer
-//     ,{
-//     cors :{
-//         origin:"http://localhost:3000/",
-//         methods:["GET","POST"]
-//     }
-// }
-);
-
-
-
-
-
-io.on("connection", (socket) => {
-    console.log(`connected io : ${socket.id}`);
-});
-io.on("disconnect", () => {
-    console.log(`connected io not `);
-});
-
 
 
 expressServer.listen(port,()=>{
@@ -50,17 +21,7 @@ expressServer.listen(port,()=>{
 })
 
 
-// client.connect()
-
-// app.use('/promo', promoRouter)
-
-
-
-app.get('/ex', (req, res) => {
+app.get('/', (req, res) => {
     res.send('server running')
 });
-
-// app.listen(port, () => {
-//     console.log('listening', port);
-// });
 
