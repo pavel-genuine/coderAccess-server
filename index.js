22fc89e777c50526e01d5da9e92d5984e1a135fc
Abdullah Al Mahmood <abdullahalmahmood500@gmail.com>
2022-08-05 02:13:43 +0600

    1st

---
+++ index.js
@@ -0,0 +1,152 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
+const port = 5000 || process.env.PORT;
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = "mongodb+srv://coderAccess:d64VFIiAegQTPsYD@cluster0.23ofj.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err=>{
+//     client.db("coderAccess").collection('profiles');
+//     console.log("DB connected")
+
+// })
+async function run() {
+    try {
+        await client.connect();
+        const challenge = client.db("coderAccess").collection('challenges');
+        //   const profile = client.db("coderAccess").collection('profiles');
+        const Algorithim = client.db("coderAccess").collection('Algorithim');
+        const Database = client.db("coderAccess").collection('Database');
+        const DS = client.db("coderAccess").collection('DS');
+
+        const problem = { title: "Sum of array " };
+        // const result = await challenge.insertOne(problem)
+        //app.get()
+        // console.log("Insert")
+
+
+
+        // app.post('/uploadproblem', async (req, res) => {
+        //     const data = req?.body;
+        //     console.log(data);
+        //     const result = await challenge.insertOne(data)
+        //     console.log(result);
+        //      res.send({status:"done"})
+        // });
+
+
+        app.get('/challengeHard', async (req, res) => {
+            const query={type:'Hard'};
+            const corsur=challenge.find(query)
+            const result=await corsur.toArray()
+            console.log(result);
+            res.send(result);
+        });
+        app.get('/challengeEasy', async (req, res) => {
+            const query={type:'Easy'};
+            const corsur=challenge.find(query)
+            const result=await corsur.toArray()
+            console.log(result);
+            res.send(result);
+        });
+
+
+
+        app.post('/Database', async (req, res) => {
+            const data = req?.body;
+            console.log(data);
+            const result = await Database.insertOne(data)
+            console.log(result);
+            res.send({ status: "done" })
+        });
+
+
+        app.post('/Algorithim', async (req, res) => {
+            const data = req?.body;
+            console.log(data);
+            const result = await Algorithim.insertOne(data)
+            console.log(result);
+            res.send({ status: "done" })
+        });
+
+
+
+        app.post('/DS', async (req, res) => {
+            const data = req?.body;
+            console.log(data);
+            const result = await DS.insertOne(data)
+            console.log(result);
+            res.send({ status: "done" })
+        });
+
+
+        app.get('/TopicAlgo',async(req,res)=>{
+            const query={}
+            const corsur= Algorithim.find(query)
+            const result=await corsur.toArray();
+            res.send(result); 
+        })
+        app.get('/TopicDS',async(req,res)=>{
+            const query={}
+            const corsur= DS.find(query)
+            const result=await corsur.toArray();
+            res.send(result); 
+        })
+        app.get('/TopicDatabase',async(req,res)=>{
+            const query={}
+            const corsur= Database.find(query)
+            const result=await corsur.toArray();
+            res.send(result); 
+        })
+
+
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.get('/uploadproblem', (req, res) => {
+    res.send("Connected");
+})
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('hello')
+})
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log('listening to port ', port);
+})
